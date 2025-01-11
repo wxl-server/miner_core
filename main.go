8aa625c1bd51970907a7c8f7f32325eefb7b4ade
wxl <1715142548@qq.com>
2025-01-11 18:35:05 +0800

    update

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	miner_core "github.com/qcq1/rpc_miner_core/kitex_gen/miner_core/itemservice"
 	"miner_core/sal/config"
 	"miner_core/sal/nacos"
-	"net"
 )
 
 func main() {
@@ -25,22 +24,11 @@ func Init(ctx context.Context) {
 
 func RunServer(ctx context.Context) {
 	serverConfig := config.Config.Server
-	options := make([]server.Option, 0)
-	if env.IsBoe() {
-		addr, err := net.ResolveTCPAddr(serverConfig.Network, serverConfig.HostPort)
-		if err != nil {
-			logger.CtxErrorf(ctx, "[Init] resolve tcp addr failed, err = %v", err)
-			panic(err)
-		}
-		options = append(options, server.WithServiceAddr(addr))
-	}
-	options = append(options, []server.Option{
-		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: serverConfig.Name}),
-		server.WithRegistry(registry.NewNacosRegistry(nacos.Register2Nacos(ctx))),
-	}...)
+
 	svr := miner_core.NewServer(
 		new(ItemServiceImpl),
-		options...,
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: serverConfig.Name}),
+		server.WithRegistry(registry.NewNacosRegistry(nacos.Register2Nacos(ctx))),
 	)
 	if err := svr.Run(); err != nil {
 		logger.CtxErrorf(ctx, "[Init] server run failed, err = %v", err)
